a625d8771efc53521db3586ae4c50a21cda288e9
Davide Andrea "Murivan" Mauro <Murivan@users.noreply.github.com>
2024-04-17 09:50:45 +0200

    Preparing support for Maven
    As Gradle does not handle non modularized jars and we have 3 (meico, jsyn, and jportaudio)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module Arpeggiatorum.main {
+    requires java.desktop;
+    requires java.logging;
+    requires jsyn;
+    requires meico;
+    requires org.controlsfx.controls;
+    requires TarsosDSP.core;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+
+//    opens arpeggiatorum to javafx.fxml;
+//    exports arpeggiatorum;
+//    opens org.openjfx to javafx.fxml;
+//    exports org.openjfx;
+}
